35112fdad888bcdbc36f2e38589a865e2441828a
Farsana kt <ffarsanakt@gmail.com>
2026-05-13 15:45:22 +0530

    updated server.js

--- server.js
+++ server.js
@@ -12,13 +12,14 @@ const allowedOrigins = [
     'https://cricket-system-frontend.vercel.app',
     'http://localhost:5173',
     'http://localhost:5174',
+    'https://player-pwa-app.vercel.app',
     
     
 ];
 
 app.use(
   cors({
-    origin: [  "http://localhost:5173","https://cricket-system-frontend.vercel.app","http://localhost:5174"],
+    origin: [  "http://localhost:5173","https://cricket-system-frontend.vercel.app","http://localhost:5174","https://player-pwa-app.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
