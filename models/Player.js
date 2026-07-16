55dfe7f7f766d4d3021f93775283d1594bbe0e53
Farsana kt <ffarsanakt@gmail.com>
2026-07-16 11:00:28 +0530

    updated player model

--- models/Player.js
+++ models/Player.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const playerSchema = new mongoose.Schema(
   {
@@ -61,5 +62,12 @@ const playerSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+playerSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export default mongoose.model("Player", playerSchema);
